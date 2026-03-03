b6fca9ba4202d21b4920adbe9e4db5ae6d7fc255
René <rene.mau@outlook.com>
2026-03-03 22:57:11 +0100

    Improvement of List Certificate page

--- Web UI/Blazor/Models/Views/ExtendedEntry.cs
+++ Web UI/Blazor/Models/Views/ExtendedEntry.cs
@@ -6,6 +6,12 @@ namespace CertificateManager.Models.Views
         public int RequestId { get; set; }
         public string CAConfig { get; set; } = string.Empty;
         public string? SerialNumber { get; set; } = string.Empty;
+        public string? RequestCommonName { get; set; } = string.Empty;
+        public string? RequestCountryRegion { get; set; } = string.Empty;
+        public string? RequestCity { get; set; } = string.Empty;
+        public string? RequestOrganization { get; set; } = string.Empty;
+        public string? RequestOrganizationUnit { get; set; } = string.Empty;
+        public string? RequestEMailAddress { get; set; } = string.Empty;
         public string? Owner { get; set; } = string.Empty;
         public string? Notes { get; set; } = string.Empty;
         public string? SubjectAlternativeNames { get; set; } = string.Empty;
--- Web UI/Blazor/Services/CertificateService.cs
+++ Web UI/Blazor/Services/CertificateService.cs
@@ -8,6 +8,34 @@ namespace CertificateManager.Services
 {
     public class CertificateService
     {
+        public AppDbContext CreateDbContext()
+        {
+            return _dbContextFactory.CreateDbContext();
+        }
+
+        public IQueryable<ExtendedEntry> GetAllCertificateEntries(AppDbContext context)
+        {
+            return context.Entry
+                .Include(e => e.SAN)
+                .Include(e => e.EKU)
+                .Select(e => new ExtendedEntry
+                {
+                    RequestId = e.RequestId,
+                    CAConfig = e.CAConfig,
+                    SerialNumber = e.SerialNumber,
+                    RequestCommonName = e.RequestCommonName,
+                    RequestCountryRegion = e.RequestCountryRegion,
+                    RequestCity = e.RequestCity,
+                    RequestOrganization = e.RequestOrganization,
+                    RequestOrganizationUnit = e.RequestOrganizationUnit,
+                    RequestEMailAddress = e.RequestEmailAddress,
+                    Owner = e.Owner,
+                    Notes = e.Notes,
+                    SubjectAlternativeNames = string.Join(", ", e.SAN.Select(s => s.SubjectAlternativeName)),
+                    EKUNames = string.Join(", ", e.EKU.Select(s => s.Name))
+                });
+        }
+
         private IDbContextFactory<AppDbContext> _dbContextFactory;
 
         public CertificateService(IDbContextFactory<AppDbContext> dbContextFactory)
@@ -37,7 +65,6 @@ namespace CertificateManager.Services
                 {
                     return entry;
                 }
-
             }
         }
 
