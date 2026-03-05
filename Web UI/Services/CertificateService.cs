f62eccf6878e8768961f6d36566245f2fe858eb2
René <rene.mau@outlook.com>
2026-03-05 09:00:05 +0100

    Included default views to display specific certificates

--- Web UI/Services/CertificateService.cs
+++ Web UI/Services/CertificateService.cs
@@ -14,7 +14,76 @@ namespace CertificateManager.Services
             return _dbContextFactory.CreateDbContext();
         }
 
-        public IQueryable<ExtendedEntry> GetCertificateEntries(AppDbContext context, int? requestId ,System.Collections.Hashtable filterht)
+        public IQueryable<ExtendedEntry> GetCertificateEntries(AppDbContext context, string searchstring, string columnname, Type table)
+        {
+            IQueryable<Entry> query = context.Entry
+                .Include(e => e.SAN)
+                .Include(e => e.EKU);
+
+            var parameter = Expression.Parameter(typeof(Entry), "e");
+            Expression? combinedFilter = null;
+
+            Expression CombineAnd(Expression? existing, Expression addition)
+                => existing == null ? addition : Expression.AndAlso(existing, addition);
+
+            if (!string.IsNullOrEmpty(columnname) && table != null && !string.IsNullOrEmpty(searchstring))
+            {
+                var Parameter = Expression.Parameter(table, "k");
+                var Property = Expression.Property(Parameter, columnname);
+                var NotNull = Expression.NotEqual(Property, Expression.Constant(null, typeof(string)));
+                var Contains = Expression.Call(
+                    Property,
+                    typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) })!,
+                    Expression.Constant(searchstring));
+                var Predicate = Expression.AndAlso(NotNull, Contains);
+                
+                var anyMethod = typeof(Enumerable)
+                    .GetMethods()
+                    .First(m => m.Name == nameof(Enumerable.Any) && m.GetParameters().Length == 2)
+                    .MakeGenericMethod(table);
+
+                var Collection = Expression.Property(parameter, table.Name);
+                if(table.Name == "EKU")
+                {
+                    var Lambda = Expression.Lambda<Func<EKU, bool>>(Predicate, Parameter);
+                    var anyCall = Expression.Call(anyMethod, Collection, Lambda);
+                    combinedFilter = CombineAnd(combinedFilter, anyCall);
+                }
+                else
+                {
+                    var Lambda = Expression.Lambda<Func<SAN, bool>>(Predicate, Parameter);
+                    var anyCall = Expression.Call(anyMethod, Collection, Lambda);
+                    combinedFilter = CombineAnd(combinedFilter, anyCall);
+                }
+
+            }
+
+            if (combinedFilter != null)
+            {
+                var lambda = Expression.Lambda<Func<Entry, bool>>(combinedFilter, parameter);
+                query = query.Where(lambda);
+            }
+
+            return query.Select(e => new ExtendedEntry
+            {
+                RequestId = e.RequestId,
+                CAConfig = e.CAConfig,
+                SerialNumber = e.SerialNumber,
+                RequestCommonName = e.RequestCommonName,
+                RequestCountryRegion = e.RequestCountryRegion,
+                RequestCity = e.RequestCity,
+                RequestOrganization = e.RequestOrganization,
+                RequestOrganizationUnit = e.RequestOrganizationUnit,
+                RequestEMailAddress = e.RequestEmailAddress,
+                Owner = e.Owner,
+                Notes = e.Notes,
+                SubjectAlternativeNames = string.Join(", ", e.SAN.Select(s => s.SubjectAlternativeName)),
+                EKUNames = string.Join(", ", e.EKU.Select(s => s.Name))
+            });
+
+        }
+
+        public IQueryable<ExtendedEntry> GetCertificateEntries(AppDbContext context, int? requestId, DateTime? expirationDate, System.Collections.Hashtable filterht)
         {
             IQueryable<Entry> query = context.Entry
                 .Include(e => e.SAN)
@@ -31,6 +100,15 @@ namespace CertificateManager.Services
                 var Property = Expression.Property(parameter, "RequestId");
                 var Equals = Expression.Equal(Property, Expression.Constant(requestId.Value));
                 combinedFilter = CombineAnd(combinedFilter, Equals);
+            }
+
+            if (expirationDate.HasValue)
+            {
+                var Property = Expression.Property(parameter, "CertificateExpirationDate");
+                var NotNull = Expression.NotEqual(Property,Expression.Constant(null, typeof(DateTime?)));
+                var Value = Expression.Property(Property, nameof(Nullable<DateTime>.Value));
+                var LessOrEqual = Expression.LessThanOrEqual(Value,Expression.Constant(expirationDate.Value));
+                combinedFilter = CombineAnd(combinedFilter, Expression.AndAlso(NotNull, LessOrEqual));
             }
 
             foreach (string key in filterht.Keys)
