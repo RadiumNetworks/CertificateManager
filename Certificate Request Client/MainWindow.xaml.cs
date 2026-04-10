cd992fc855883704fcccd50069c0d0869aa4ea8f
René <rene.mau@outlook.com>
2026-04-10 16:47:48 +0200

    Parametrisation of request client

--- Certificate Request Client/MainWindow.xaml.cs
+++ Certificate Request Client/MainWindow.xaml.cs
@@ -1,22 +1,13 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using Microsoft.Extensions.Configuration;
+using Microsoft.UI.Xaml;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.Json;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage;
 
 namespace CertificateManager
 {
@@ -24,18 +15,46 @@ namespace CertificateManager
     {
         private static readonly HttpClient _httpClient;
 
+        private static string _apihost;
+
+        private static List<string> _certificateAuthorities;
+
+        public ViewModel viewModel { get; } = new();
+
         static MainWindow()
         {
+            string userPath = ApplicationData.Current.LocalFolder.Path;
+            string userconfigPath = Path.Combine(userPath, "appsettings.user.json");
+            string applicationPath = AppContext.BaseDirectory;
+            string appconfigPath = Path.Combine(applicationPath, "appsettings.json");
+            string jsonContent = File.ReadAllText(appconfigPath);
+
+            Directory.CreateDirectory(userPath);
             var config = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", optional: false)
+                .SetBasePath(applicationPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
                 .Build();
 
-            var apiHost = config["APIHost:URI"] ?? "https://localhost:5301";
+            if (File.Exists(userconfigPath))
+            {
+                config = new ConfigurationBuilder()
+                    .SetBasePath(userPath)
+                    .AddJsonFile("appsettings.user.json", optional: false, reloadOnChange: false)
+                    .Build();
+            }
+            else
+            {
+                File.WriteAllText(userconfigPath, jsonContent);
+            }
+
+            _apihost = config["APIHost:URI"] ?? "https://localhost:5301";
+
+            _certificateAuthorities = config.GetSection("CertificateAuthorities")
+                .GetChildren().Select(c => c.Value!).ToList();
 
             _httpClient = new HttpClient
             {
-                BaseAddress = new Uri(apiHost),
+                BaseAddress = new Uri(_apihost),
                 Timeout = TimeSpan.FromSeconds(10)
             };
         }
@@ -48,35 +67,46 @@ namespace CertificateManager
         public MainWindow()
         {
             InitializeComponent();
+            viewModel.APIString = $"API located at {_apihost}";
 
-            
+            CAConfigCombo.ItemsSource = _certificateAuthorities;
+            if (_certificateAuthorities.Count > 0)
+                CAConfigCombo.SelectedIndex = 0;
         }
 
         private async void ParseButton_Click(object sender, RoutedEventArgs e)
         {
             var input = Base64RequestInput.Text?.Trim();
             var request = new { Input = input };
-            var response = await _httpClient.PostAsJsonAsync("api/parse", request);
-            response.EnsureSuccessStatusCode();
-
-            var result = await response.Content.ReadFromJsonAsync<Response>(_jsonOptions);
-            if (result != null)
+            try
             {
-                var data = result.Result.ToString().Split(";");
-                if (data.Length > 0)
-                {
-                    if (data[0] == "Parsed")
-                    {
-                        ParsedDataOutput.Text = data[1];
-                        ChallengeDataOutput.Text = data[2];
-                    }
-                    else
-                    {
-                        ParsedDataOutput.Text = data[3];
-                    }
-                }
+                var response = await _httpClient.PostAsJsonAsync("api/parse", request);
+                response.EnsureSuccessStatusCode();
 
+                var result = await response.Content.ReadFromJsonAsync<Response>(_jsonOptions);
+                if (result != null)
+                {
+                    var data = result.Result.ToString().Split(";");
+                    if (data.Length > 0)
+                    {
+                        if (data[0] == "Parsed")
+                        {
+                            ParsedDataOutput.Text = data[1];
+                            ChallengeDataOutput.Text = data[2];
+                        }
+                        else
+                        {
+                            ParsedDataOutput.Text = data[3];
+                        }
+                    }
+
+                }
             }
+            catch (Exception ex)
+            {
+                ChallengeDataOutput.Text = ex.Message;
+            }
+            
 
         }
 
@@ -84,11 +114,19 @@ namespace CertificateManager
         {
             var input = Base64RequestInput.Text?.Trim();
             var request = new { Input = input };
-            var response = await _httpClient.PostAsJsonAsync("api/submit", request);
-            response.EnsureSuccessStatusCode();
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("api/submit", request);
+                response.EnsureSuccessStatusCode();
 
-            var result = await response.Content.ReadFromJsonAsync<Response>(_jsonOptions);
-            OutputBox.Text = result?.Result ?? "No response received";
+                var result = await response.Content.ReadFromJsonAsync<Response>(_jsonOptions);
+                OutputBox.Text = result?.Result ?? "No response received";
+            }
+            catch (Exception ex)
+            {
+                OutputBox.Text = ex.Message;
+            }
+
         }
 
         private void RetrieveButton_Click(object sender, RoutedEventArgs e)
---
+++ Certificate Request Client/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertificateManager
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public string _APIString = string.Empty;
+
+        public string APIString
+        {
+            get => _APIString;
+            set
+            {
+                if (_APIString != value)
+                {
+                    _APIString = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(APIString)));
+
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
